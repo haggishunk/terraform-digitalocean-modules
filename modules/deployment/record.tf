86235ebaae35b2a072a8d9169f82113204972001
haggishunk <travis@mattera.io>
2022-10-22 20:51:20 -0700

    Add a dns record for the private vpc ip address.

--- modules/deployment/record.tf
+++ modules/deployment/record.tf
@@ -5,3 +5,11 @@ resource "digitalocean_record" "this" {
   name   = var.name
   value  = var.reserved_ip_enabled ? digitalocean_reserved_ip.this.0.ip_address : digitalocean_droplet.this.ipv4_address
 }
+
+resource "digitalocean_record" "this_internal" {
+  count  = var.dns_enabled ? 1 : 0
+  domain = var.domain
+  type   = "A"
+  name   = "${var.name}-internal"
+  value  = digitalocean_droplet.this.ipv4_address_private
+}
