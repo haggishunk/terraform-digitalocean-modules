59904ebab8172ec0a5fcc3150d4af7cd94f887bb
Travis Mattera <travis@mattera.io>
2023-10-27 09:21:10 -0700

    Added vpc resource

---
+++ modules/vpc/outputs.tf
@@ -0,0 +1,15 @@
+output "id" {
+  value = digitalocean_vpc.this.id
+}
+
+output "urn" {
+  value = digitalocean_vpc.this.urn
+}
+
+output "default" {
+  value = digitalocean_vpc.this.default
+}
+
+output "created_at" {
+  value = digitalocean_vpc.this.created_at
+}
